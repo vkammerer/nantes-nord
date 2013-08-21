5992423b1342031356bebafc3543fda01d3b4a25
nielk <oger.alexandre@gmail.com>
2013-08-21 11:35:32 +0200

    server.js configured

--- tools/server.js
+++ tools/server.js
@@ -1,8 +1,7 @@
 var express = require('express'),
 	app = express();
 
-app.use(express.static(__dirname + '/../src/'));
-app.use('/bower_components', express.static(__dirname + '/../bower_components'));
+app.use(express.static(__dirname + '/../dist/'));
 app.use(express.bodyParser());
 
 var port = process.env.PORT || 8000;
