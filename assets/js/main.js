08efc0b70ebcdf14cbbe58e8804c53ca9eda8257
xseignard <xavier.seignard@gmail.com>
2013-08-29 13:21:29 +0200

    scroll again

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,17 +102,17 @@ if ( typeof define === 'function' && define.amd ) {
         $('#section-02').addClass('section-video-hide');
         $('#section-02-player').addClass('section-video-show');
     });
-
     var scrollAnimation = function(picto, section) {
         $(picto).click(function (){
-            $('html').animate({
+            $('body', 'html').animate({
                 scrollTop: $(section).offset().top-100
             }, 1000);
         });
     };
 
-    for(var i=1; i>9; i++) {
-        scrollAnimation("#picto"+i);
+    for(var i=1; i<9; i++) {
+        scrollAnimation('#picto'+i, '#section-0'+i);
+        console.log('#picto'+i);
     }
 });;$(document).ready(function() {
     $('.btn-transition-01').click(function() {
