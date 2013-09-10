e7aa4321103971593ccb8df4d228f6cdcbab3fbb
nielk <oger.alexandre@gmail.com>
2013-09-10 16:30:36 +0200

    scrollSection pour menu smartphone

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,11 +30,19 @@ $(document).ready(function() {
 });;
 $(document).ready(function (){
 
+    // alert($(window).width());
+
     var scrollAnimation = function(picto, section) {
         $(picto).click(function (){
+            if($(window).width()>764) {
             $('body,html').animate({
                 scrollTop: $(section).offset().top - $('html').height() * .1
             }, 1000);
+        } else {
+            $('body,html').animate({
+                scrollTop: $(section).offset().top
+            }, 1000);
+        }
         });
     };
 
